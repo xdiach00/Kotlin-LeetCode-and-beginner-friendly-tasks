4da6513a2a857ef53772c32e20fa70259795f93a
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2022-11-24 15:57:20 +0100

    Add exercise2 to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
-import tasks.exercise1
+import tasks.*
 
 fun main() {
     print("Enter task number: ")
     when (readln().toInt()) {
         1 -> exercise1()
+        2 -> exercise2()
         else -> println("No such task")
     }
 }
