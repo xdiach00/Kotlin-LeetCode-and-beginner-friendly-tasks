96af75460c52f33ddf216528d74aedd32c259d54
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2022-11-24 15:40:19 +0100

    Add first task solution without temp var

--- src/main/kotlin/tasks/Exercise1.kt
+++ src/main/kotlin/tasks/Exercise1.kt
@@ -4,13 +4,23 @@
 package tasks
 
 // with a temporary variable
-fun Exercise1() {
+//fun exercise1() {
+//    print("Enter a: ")
+//    var a = readln().toInt()
+//    print("Enter b: ")
+//    var b = readln().toInt()
+//    val c = a
+//    a = b
+//    b = c
+//    println("a is $a, b is $b")
+//}
+
+// without a temporary variable using inline fun <T> T.also(block: (T) -> Unit): T
+fun exercise1() {
     print("Enter a: ")
     var a = readln().toInt()
     print("Enter b: ")
     var b = readln().toInt()
-    val c = a
-    a = b
-    b = c
+    a = b.also { b = a }
     println("a is $a, b is $b")
 }
