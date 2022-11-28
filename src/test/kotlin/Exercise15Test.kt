4c379ee82b259691bcf1ae86fd54eb98af9b5e1a
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2022-11-28 15:31:38 +0100

    Add unit tests for exercise 15

---
+++ src/test/kotlin/Exercise15Test.kt
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import tasks.exercise15
+
+class Exercise15Test {
+
+    @Test
+    @DisplayName("500 minutes")
+    fun test1() {
+        val result = exercise15(500)
+        assertEquals("Total amount: 5.0$", result)
+    }
+
+    @Test
+    @DisplayName("800 minutes")
+    fun test2() {
+        val result = exercise15(800)
+        assertEquals("Total amount: 7.4$", result)
+    }
+
+    @Test
+    @DisplayName("1000 minutes")
+    fun test3() {
+        val result= exercise15(1000)
+        assertEquals("Total amount: 8.4$", result)
+    }
+}
